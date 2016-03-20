a772a0f182daf5cc298fa552a36528086e12e3bb
Musab M. Jafri <musabmjafri@users.noreply.github.com>
2016-03-20 09:33:31 +0400

    Added missing comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,3 +1,10 @@
+## Submission by Musab M. Jafri
+
+## The method returns a cacheable matrix with the following properties:
+## get - returns the matrix
+## set - sets a new version of the matrix
+## getinverse - gets the cached inverse of the matrix
+## setinverse - sets a new inverse value for the matrix
 makeCacheMatrix <- function(x = matrix()) {
     i <- NULL
     set <- function(y) {
@@ -12,6 +19,7 @@ makeCacheMatrix <- function(x = matrix()) {
          getinverse = getinverse)
 }
 
+## The method returns the inverse of the provided cacheable matrix
 cacheSolve <- function(x, ...) {
     i <- x$getinverse()
     if(!is.null(i)) {
